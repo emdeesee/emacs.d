1eb1d83eac5d4b964e75ce2232bbc59942cad4f1
Michael Cornelius <michael@ninthorder.com>
2015-12-09 22:14:00 -0600

    Don't switch to other frame when switching buffers
    See documentation for variable

--- elisp/mdc-ido.el
+++ elisp/mdc-ido.el
@@ -2,6 +2,7 @@
 (ido-mode t)
 
 (setq ido-create-new-buffer 'always
-      ido-use-filename-at-point 'guess)
+      ido-use-filename-at-point 'guess
+      ido-default-buffer-method 'maybe-frame)
 
 (provide 'mdc-ido)
