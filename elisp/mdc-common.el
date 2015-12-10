e54181c8cbfb1db037b9f05b0301ae8f34c50bb4
Michael Cornelius <michael@ninthorder.com>
2015-12-09 22:10:44 -0600

    handy function
    Put the file name of the current buffer, or part of it, on the kill ring
    for yanking somewhere else.

--- elisp/mdc-common.el
+++ elisp/mdc-common.el
@@ -49,4 +49,19 @@ form as the second argument in the current.
           "pariatur. Excepteur sint occaecat cupidatat non proident, sunt in "
           "culpa qui officia deserunt mollit anim id est laborum."))
 
+(defun copy-buffer-file-name-as-kill (choice)
+  "Copy the buffer file name to the kill-ring"
+  (interactive "cCopy Buffer Name (F) Full, (D) Directory, (N) Name")
+  (let ((new-kill-string)
+	(name (if (eq major-mode 'dired-mode)
+		  (dired-get-filename)
+		(or (buffer-file-name) ""))))
+    (cond ((eq choice ?f) (setq new-kill-string name))
+	  ((eq choice ?d) (setq new-kill-string (file-name-directory name)))
+	  ((eq choice ?n) (setq new-kill-string (file-name-nondirectory name)))
+	  (t (message "Quit")))
+    (when new-kill-string
+      (message "%s copied" new-kill-string)
+      (kill-new new-kill-string))))
+
 (provide 'mdc-common)
