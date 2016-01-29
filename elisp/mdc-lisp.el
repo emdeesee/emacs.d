4300f9b0ef5d4a8a2c214ab49b23bae12f906e79
Michael Cornelius <michael@dsndata.com>
2016-01-29 11:01:14 -0600

    Use slime
    I'd like to do some common lisp hacking.

--- elisp/mdc-lisp.el
+++ elisp/mdc-lisp.el
@@ -3,6 +3,10 @@
 
 (use-package clojure-mode :ensure t)
 (use-package cider :ensure t)
+(use-package slime
+  :ensure t
+  :config (progn
+            (setq inferior-lisp-program "/usr/bin/sbcl")))
 
 (setq mdc-lisp-modes
       '(emacs-lisp-mode lisp-mode clojure-mode))
