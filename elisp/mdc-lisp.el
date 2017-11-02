e71bb8a315a275ca2092bfe9debc5a95ca607fdc
Michael Cornelius <michael@dsndata.com>
2017-11-02 11:08:59 -0500

    Improve handling of alternate Lisp programs

--- elisp/mdc-lisp.el
+++ elisp/mdc-lisp.el
@@ -1,12 +1,16 @@
 (provide 'mdc-lisp)
 (require 'mdc-common)
+(require 'mdc-local nil :noerror)
 
 (use-package clojure-mode :ensure t)
 (use-package cider :ensure t)
 (use-package slime
   :ensure t
   :config (progn
-            (setq inferior-lisp-program "/usr/bin/sbcl")
+            (setq inferior-lisp-program
+                  (if (fboundp 'mdc/*inferior-lisp-program*)
+                      mdc/*inferior-lisp-program*
+                    "/usr/bin/sbcl"))
             (slime-setup '(slime-fancy slime-asdf))))
 
 (setq mdc-lisp-modes
