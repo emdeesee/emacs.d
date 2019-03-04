f5ef56a2ae231365c4567a173f0c47ea47f5667d
Michael Cornelius <michael@dsndata.com>
2019-03-04 13:54:50 -0600

    Make a keymap for git commands.

--- elisp/mdc-magit.el
+++ elisp/mdc-magit.el
@@ -1,4 +1,15 @@
 (provide 'mdc-magit)
 (use-package magit
-  :ensure t
-  :bind ("C-c g" . magit-status))
+  :ensure t)
+
+;; TODO Limit keys to appropriate buffers
+(defconst my/magit-prefix "\C-cg")
+
+(setq my/magit-keymap (let ((map (make-sparse-keymap))
+                            (keys `(("!" . magit-status)
+                                    ("?" . magit-file-popup)
+                                    ("b" . magit-blame))))
+                        (dolist (k keys map)
+                          (define-key map (car k) (cdr k)))))
+
+(global-set-key my/magit-prefix my/magit-keymap)
