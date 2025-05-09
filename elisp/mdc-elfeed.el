64a59967bc21e4b48b9c693504404d9121430482
Michael Cornelius <michael@ninthorder.com>
2025-05-08 20:47:05 -0700

    Update feeds when feeds file changes.

--- elisp/mdc-elfeed.el
+++ elisp/mdc-elfeed.el
@@ -2,9 +2,25 @@
 
 (use-package elfeed)
 
-(let ((feeds-file (expand-file-name "elfeed-feeds.el" (or (getenv "XDG_CONFIG_HOME")
-                                                          "~/.local"))))
-  (when (file-exists-p feeds-file)
-    (load-file feeds-file)))
+(defvar mdc/elfeed-feeds-file
+  (expand-file-name "elfeed-feeds.el" (or (getenv "XDG_CONFIG_HOME") "~/.local")))
+
+(when (file-exists-p mdc/elfeed-feeds-file)
+  (load-file mdc/elfeed-feeds-file))
+
+(defun mdc/reload-elfeed-feeds (_event)
+  "Reload elfeed-feeds from file when it changes."
+  (message "Reloading elfeed feeds from %s..." mdc/elfeed-feeds-file)
+  (load-file mdc/elfeed-feeds-file)
+  (when (get-buffer "*elfeed-search*")
+    (with-current-buffer "*elfeed-search*"
+      (elfeed-search-update--force)))
+  (message "Feeds reloaded."))
+
+(when (and (file-exists-p mdc/elfeed-feeds-file)
+           (require 'filenotify nil t))
+  (file-notify-add-watch mdc/elfeed-feeds-file
+                         '(change)
+                         #'mdc/reload-elfeed-feeds))
 
 (setq-default elfeed-search-filter "@2-weeks-ago +unread +frontpage")
